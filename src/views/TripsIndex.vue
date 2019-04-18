43fa01ec3423bf07cdf20242036c89e33797208a
Lindsey Katz <katz.lindsey@gmail.com>
2019-04-17 21:32:11 -0500

    Edit Trips page to show all trips for current user

--- src/views/TripsIndex.vue
+++ src/views/TripsIndex.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="trips-index">
     <h3>When a user logs in, this page will show all of that user's itineraries, past and upcoming.</h3>
+    My Trips:
+    <div v-for="trip in trips">
+      <h2>City_id: {{ trip.city_id }}</h2>
+      <p>Name: {{ trip.trip_name }}</p>
+      <p>Start Date: {{ trip.start_date }}</p>
+      <p>End Date: {{ trip.end_date }}</p>
+    </div>
   </div>
 </template>
-<script></script>
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      trips: []
+    };
+  },
+  created: function() {
+    // RUBY WEB REQUEST
+    // response = HTTP.get("/api/products")
+    // @recipes = response.data;
+    // JAVASCRIPT WEB REQUEST
+    axios.get("/api/trips").then(response => {
+      this.trips = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
